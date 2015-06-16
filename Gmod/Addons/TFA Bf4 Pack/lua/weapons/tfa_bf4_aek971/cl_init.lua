9b3362a09f442b7402949978b9fba45f997ef221
Matthew <mattymoo142@gmail.com>
2015-06-16 17:36:21 +0100

    Removed unneeded folder

--- Gmod/Addons/TFA Bf4 Pack/lua/weapons/tfa_bf4_aek971/cl_init.lua
+++
@@ -1,8 +0,0 @@
-include("shared.lua")
-
-SWEP.PrintName				= "AEK-971"			// 'Nice' Weapon name (Shown on HUD)	
-SWEP.Slot				= 3				// Slot in the weapon selection menu
-SWEP.SlotPos				= 3				// Position in the slot
-SWEP.DrawAmmo				= 				true// Should draw the default HL2 ammo counter				// Should draw the default crosshair
-SWEP.DrawWeaponInfoBox			= 				true// Should draw the weapon info box
-SWEP.BounceWeaponIcon   		= 				false// Should the weapon icon bounce?
--- Gmod/Gamemode/gmodgg/gamemode/shared.lua
+++
@@ -1,7 +0,0 @@
-GM.Name 	= "Gmod Gun Game - SWEPS"
-GM.Author 	= "Mattymoo142 & Kwigg & Ad1712"
-GM.Email 	= "mattymoo142@gmail.com"
-GM.Website 	= "http://steamcommunity.com/id/Kwigg/"
-
-
-
