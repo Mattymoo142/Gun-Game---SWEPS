9741c04d78d73029056a692df5bea52104749150
Mattymoo142 <mattymoo142@gmail.com>
2015-06-18 16:12:39 +0100

    Updated 18/06/2015

--- Gmod/gmodgg/gamemode/shared.lua
+++ Gmod/gmodgg/gamemode/shared.lua
@@ -1,4 +1,4 @@
-GM.Name 	= "Gmod Gun Game - SWEPS"
+GM.Name 	= "Gmod Gun Master"
 GM.Author 	= "Mattymoo142 & Kwigg & Ad1712"
 GM.Email 	= "mattymoo142@gmail.com"
 GM.Website 	= "http://steamcommunity.com/id/Kwigg/"
